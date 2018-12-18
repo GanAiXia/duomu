eb05dcb24e757fd901ad67631a32168727167268
GanAiXia <ganaixia@foxmail.com>
2018-12-18 14:47:11 +0800

    Add files via upload

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,2 +1,11 @@
 import resetCss from '../css/reset.css'
 import indexCss from '../css/index.scss'
+
+{
+    //得到手机屏幕的宽度
+    let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    //得到html的Dom元素
+    let htmlDom = document.getElementsByTagName('html')[0];
+    //设置根元素字体大小
+    htmlDom.style.fontSize= htmlWidth/20 + 'px';
+}
